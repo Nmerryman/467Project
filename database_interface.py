62356ef8e6fdd4b3b36d1713c9c347f6bb1f9c79
Nmerryman <nmerryman9001@gmail.com>
2023-11-20 03:17:04 -0600

    Fixed the custom database interface and added some tests/data generations

--- database_interface.py
+++ database_interface.py
@@ -6,17 +6,18 @@ from sqlalchemy import String, Integer, ForeignKey
 from typing import List, Optional
 from datetime import datetime
 
-# import legacy_interface
+from sqlalchemy import insert
+
+import legacy_interface
 
 
-class Base(DeclarativeBase, MappedAsDataclass):
-    pass
+Base = legacy_interface.Base    # Make sure we use the same Base as the other tables to avoid any issues
 
 
 class Customer(Base):                   # Currently only stores information needed to contact + ship
     __tablename__ = "customers"
 
-    id: Mapped[int] = mapped_column(primary_key=True, init=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]                   # Client name
     email: Mapped[str]                  # Email
     address1: Mapped[str]               # Client street address
@@ -26,37 +27,38 @@ class Customer(Base):                   # Currently only stores information need
 class Order(Base):                                                                  # Source of the order invoice
     __tablename__ = "orders"
 
-    id: Mapped[int] = mapped_column(primary_key=True, init=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
     customer_id: Mapped[int] = mapped_column(ForeignKey("customers.id"))
-    status: Mapped[str]                                                             # Something like [received, awaiting_stock, in_progress, shipped]
-    created: Mapped[datetime] = mapped_column(default_factory=datetime.utcnow)      # When was the order received
-    finished: Mapped[Optional[datetime]] = mapped_column(default_factory=datetime)  # When was the order completed
+    status: Mapped[str]                                                     # Something like [received, awaiting_stock, in_progress, shipped]
+    created: Mapped[datetime]                                               # When was the order received
+    finished: Mapped[Optional[datetime]]                                    # When was the order completed
     pricing_model_id: Mapped[int] = mapped_column(ForeignKey("fees.id"))
-    calculated_cost: Mapped[float]                                                  # Just store the calculated cost for ease
+    calculated_cost: Mapped[float]                                          # Just store the calculated cost for ease
 
 
 class OrderItem(Base):                                                  # Each item included in an order
     __tablename__ = "order_items"
 
-    id: Mapped[int] = mapped_column(primary_key=True, init=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
     order_id: Mapped[int] = mapped_column(ForeignKey("orders.id"))
     item_id: Mapped[int] = mapped_column(ForeignKey("inventory.id"))
     quantity: Mapped[int]                                               # Num of items in the order
     status: Mapped[str]                                                 # Status such as [missing_stock, collecting, ready]
+    cost: Mapped[float]                                                 # Store a precalculated value for cost
 
 
 class Inventory(Base):                                              # Stores current state of the stock of every item in inventory
     __tablename__ = "inventory"
 
-    id: Mapped[int] = mapped_column(primary_key=True, init=False)
-    legacy_id: Mapped[int] = mapped_column(ForeignKey("parts.id"))  # Id of item in legacy database
+    id: Mapped[int] = mapped_column(primary_key=True)
+    legacy_id: Mapped[int] = mapped_column(ForeignKey("parts.number"))  # Id of item in legacy database
     stock: Mapped[int]                                              # Current num of available stock
 
 
 class FeeBrackets(Base):
     __tablename__ = "fee_brackets"
 
-    id: Mapped[int] = mapped_column(primary_key=True, init=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]                               # Bracket info
     min_weight: Mapped[Optional[float]]             # Min and max weights for the current bracket type
     max_weight: Mapped[Optional[float]]
@@ -65,7 +67,7 @@ class FeeBrackets(Base):
 class Fees(Base):
     __tablename__ = "fees"
 
-    id: Mapped[int] = mapped_column(primary_key=True, init=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
     description: Mapped[str]                                                # Helper text (maybe delete and just use fees.bracket?)
     bracket: Mapped[int] = mapped_column(ForeignKey("fee_brackets.id"))     # What bracket do we use to properly apply our fees
     base_charge: Mapped[float]                                              # Base order charge
@@ -75,10 +77,36 @@ class Fees(Base):
     # shipping_b: Mapped[float]
 
 
+ENGINE = create_engine("sqlite:///test.db")
+Base.metadata.create_all(ENGINE)
 
 
-ENGINE = create_engine("sqlite:///data.db")
+def gen_dev_customer():
+    # Generates a test user
+    with Session(ENGINE) as session:
+        session.execute(insert(Customer).values(name="test", email="a@b.c", address1="123 street", address2="Bajookie land"))
+        session.commit()
+
+
+def sec(statement, commit=True):
+    """
+    Session - Execute - Commit
+    Shorthand because I'm lazy
+    """
+    with Session(ENGINE) as session:
+        res = session.execute(statement)
+        if commit:
+            session.commit()
+    return res
+
+
+def main():
+    # gen_dev_customer()
+    stmt = insert(Customer).values(name="a", email="B", address1="1", address2="2")
+    sec(stmt)
 
 
 
+if __name__ == '__main__':
+    main()
 
---
+++ testing.py
@@ -0,0 +1,139 @@
+import faker
+import random
+from legacy_interface import ask_legacy, LegacyParts
+from database_interface import *
+
+from sqlalchemy import Select
+
+fake = faker.Faker()
+
+gen_counts = {"Customers": 50, "FeeBrackets": 4, "Fees": 30, "Inventory": 50, "OrderItem": 100, "Order": 10}
+status_options = ["pre", "in", "post"]
+
+
+def test_legacy():
+    """
+    Testing to make sure that we can connect to the legacy database and get valid results
+    """
+    query = Select(LegacyParts).filter(LegacyParts.price > 100)
+    res = ask_legacy(query)
+    assert len(res) > 10, res
+
+    query = Select(LegacyParts).filter(LegacyParts.price < 100)
+    res = ask_legacy(query)
+    assert len(res) > 10, res
+
+    query = Select(LegacyParts).filter(LegacyParts.price > 10000)
+    res = ask_legacy(query)
+    assert len(res) == 0, res
+
+    print("Passed Legacy test")
+
+
+def drop_tables():
+    """
+    Drop all tables
+    """
+    Base.metadata.drop_all(ENGINE)
+
+
+def make_tables():
+    """
+    Make sure all tables exist
+    """
+    Base.metadata.create_all(ENGINE)
+
+
+def add_customer():
+    for _ in range(gen_counts["Customers"]):
+        address = fake.address().split('\n')
+        stmt = insert(Customer).values(name=fake.name(), email=fake.email(), address1=address[0], address2=address[1])
+        sec(stmt)
+    print("Added Customers")
+
+
+def add_fee_bracket():
+    # Clear the table because we don't really want to randomly generate extra rows
+    Base.metadata.drop_all(ENGINE, tables=[FeeBrackets.__table__])
+    Base.metadata.create_all(ENGINE)
+
+    steps = gen_counts["FeeBrackets"]
+    size = 50
+    start = 0
+    after = random.random() * size
+
+    for a in range(steps):
+        stmt = insert(FeeBrackets).values(name=f"Step: {a}", min_weight=start, max_weight=after)
+        sec(stmt)
+
+        start = after
+        after = random.random() * size
+
+    print("Added FeeBrackets")
+
+
+def add_fees():
+    Base.metadata.drop_all(ENGINE, tables=[Fees.__table__])
+    Base.metadata.create_all(ENGINE)
+
+    for _ in range(gen_counts["Fees"]):
+        stmt = insert(Fees).values(description=fake.text()[0:100], bracket=random.randint(1, gen_counts["FeeBrackets"]),
+                                   base_charge=random.random()*500, weight_m=random.random()*50,
+                                   weight_b=random.random()*50)
+        sec(stmt)
+
+    print("Added Fees")
+
+
+def add_inventory():
+    Base.metadata.drop_all(ENGINE, tables=[Inventory.__table__])
+    Base.metadata.create_all(ENGINE)
+
+    for _ in range(gen_counts["Inventory"]):
+        stmt = insert(Inventory).values(legacy_id=random.randint(1, 100), stock=random.randint(2, 200))
+        sec(stmt)
+
+    print("Added Inventory")
+
+
+def add_order():
+    Base.metadata.drop_all(ENGINE, tables=[Order.__table__])
+    Base.metadata.create_all(ENGINE)
+
+    for _ in range(gen_counts["Order"]):
+        finished_date = fake.date_time_between(start_date="now", end_date="+1y") if random.random() > 0.5 else None
+        stmt = insert(Order).values(customer_id=random.randint(1, gen_counts["Customers"]), status=random.choice(status_options),
+                                    created=fake.date_time_between(start_date="-1y", end_date="now"), finished=finished_date,
+                                    pricing_model_id=random.randint(1, gen_counts["Fees"]), calculated_cost=random.random() * 20_000)
+        sec(stmt)
+
+    print("Added Order")
+
+
+def add_order_item():
+    Base.metadata.drop_all(ENGINE, tables=[OrderItem.__table__])
+    Base.metadata.create_all(ENGINE)
+
+    for _ in range(gen_counts["OrderItem"]):
+        stmt = insert(OrderItem).values(order_id=random.randint(1, gen_counts["Order"]), item_id=random.randint(1, gen_counts["Inventory"]),
+                                        quantity=random.randint(1, 20), status=random.choice(status_options),
+                                        cost=random.random()*50)
+        sec(stmt)
+
+    print("Added OrderItems")
+
+
+def gen_tables():
+    # Just do all at once in order
+    drop_tables()
+    make_tables()
+    add_customer()
+    add_fee_bracket()
+    add_fees()
+    add_inventory()
+    add_order()
+    add_order_item()
+
+
+if __name__ == '__main__':
+    gen_tables()
