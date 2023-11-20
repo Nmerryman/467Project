5da16ace131fbef79646fd8e00651d9baa397590
jwilson9567 <josephwilson9567@example.com>
2023-11-20 16:37:31 -0600

    Added templates to display on html

--- server.py
+++ server.py
@@ -1,12 +1,16 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from legacy_interface import LegacyParts, ask_legacy, Select
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def default():
-    return "ok"
+    # Get your data (replace this with your actual data fetching code)
+    data = ask_legacy(Select(LegacyParts).filter(LegacyParts.price >= 100))
+
+    # Render the HTML template and pass the data to it
+    return render_template('index.html', data=data)
+
     
 
 
