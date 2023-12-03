6dd5e22b5d2f256ae31bfde524342ad47bd1d09e
Spencer Hix <spencerhix@gmail.com>
2023-12-03 14:23:23 -0600

    Moved OnClick() function to inventory_add.js and store_front.js

--- static/js/inventory_add.js
+++ static/js/inventory_add.js
@@ -2,5 +2,11 @@ function handleInput(event) {
     console.log("Handle input doesn't do anything yet")
 }
 
-
-
+function onClick(el) {
+    const button = document.getElementById(el);
+    var clicks = 0;
+    clicks += 1
+    if(clicks > 0) {
+      button.removeAttribute("disabled");
+    }
+  }
